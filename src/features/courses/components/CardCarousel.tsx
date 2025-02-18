85f3d220d49bb277814090b1ba15441d5d14eb54
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2025-02-19 06:18:28 +0900

    update :

--- src/features/courses/components/CardCarousel.tsx
+++ src/features/courses/components/CardCarousel.tsx
@@ -12,18 +12,18 @@ import SlideCard from "./SlideCard";
 
 export default function CardCarousel({ courses }: CardCarouselProps) {
   return (
-    <div className="relative w-full max-w-4xl mx-auto">
+    <div className="relative w-full max-w-[1150px] h-2/3 mx-auto">
       {/* カスタムナビゲーションボタン */}
       <Button
         variant="ghost"
-        className="absolute left-[-80px] z-10 top-1/2 transform -translate-y-1/2 bg-white/90"
+        className="absolute left-[-30px] z-10 top-1/2 transform -translate-y-1/2 bg-white/90"
         id="prevButton"
       >
         <ChevronLeft className="w-6 h-6" />
       </Button>
       <Button
         variant="ghost"
-        className="absolute right-[-80px] z-10 top-1/2 transform -translate-y-1/2 bg-white/90"
+        className="absolute right-[-30px] z-10 top-1/2 transform -translate-y-1/2 bg-white/90"
         id="nextButton"
       >
         <ChevronRight className="w-6 h-6" />
@@ -31,16 +31,16 @@ export default function CardCarousel({ courses }: CardCarouselProps) {
 
       <Swiper
         modules={[Navigation, Pagination]}
-        spaceBetween={20}
+        spaceBetween={1}
         slidesPerView={1.5}
         breakpoints={{
           640: { slidesPerView: 2 },
-          1024: { slidesPerView: 3 },
+          1024: { slidesPerView: 4 },
         }}
         navigation={{ prevEl: "#prevButton", nextEl: "#nextButton" }}
         pagination={{ clickable: true }}
         loop={true}
-        className="pb-8"
+        mousewheel={true}
       >
         {courses.map((course) => (
           <SwiperSlide key={course.id}>
@@ -48,6 +48,12 @@ export default function CardCarousel({ courses }: CardCarouselProps) {
           </SwiperSlide>
         ))}
       </Swiper>
+      {/* 
+      <div
+        id="customPagination"
+        className="swiper-pagination absolute bottom-[20px] left-1/2 transform -translate-x-1/2"
+      />
+       */}
     </div>
   );
 }
