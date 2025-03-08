c1bf7b05e4acd633b153ab53c89799dfc7164b6d
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2025-03-09 00:36:03 +0900

    ✨ createProblem

--- src/app/api/[[...route]]/courses/problems/index.ts
+++ src/app/api/[[...route]]/courses/problems/index.ts
@@ -1,7 +1,9 @@
 import { Hono } from "hono";
+import { createProblem } from "./createProblem";
 import { getProblemList } from "./getProblemList";
 
 const problems = new Hono()
   .get("/", ...getProblemList)
+  .post("/", ...createProblem);
 
 export default problems;
