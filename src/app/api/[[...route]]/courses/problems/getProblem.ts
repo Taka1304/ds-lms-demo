323daeb279a6d38b7604e5ac6a1a2099eda97fee
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2025-03-13 15:08:56 +0900

    ✨ getProblem

---
+++ src/app/api/[[...route]]/courses/problems/getProblem.ts
@@ -0,0 +1,57 @@
+import { withSession } from "@/app/api/[[...route]]/middleware/auth";
+import { prisma } from "@/lib/prisma";
+import { zValidator } from "@hono/zod-validator";
+import { createFactory } from "hono/factory";
+import type { Session } from "next-auth";
+import { z } from "zod";
+
+type Variables = {
+  session: Session;
+};
+
+const factory = createFactory<{ Variables: Variables }>();
+
+export const getProblem = factory.createHandlers(
+  withSession,
+  zValidator(
+    "param",
+    z.object({
+      problem_id: z.string().cuid(),
+    }),
+  ),
+  async (c) => {
+    const { problem_id } = c.req.valid("param");
+
+    const session = c.get("session");
+    const isAdmin = session?.user.role === "ADMIN";
+
+    try {
+      const data = await prisma.problem.findFirst({
+        include: {
+          createdBy: isAdmin,
+          updatedBy: isAdmin,
+          tags: true,
+          testCases: true,
+          submissions: isAdmin
+            ? true
+            : {
+                where: {
+                  userId: session?.user.id,
+                },
+              },
+        },
+        where: {
+          id: problem_id,
+        },
+      });
+
+      if (!data) {
+        return c.json({ error: "指定された問題が見つかりませんでした" }, 404);
+      }
+      return c.json(data);
+    } catch (error) {
+      console.error("データの取得中にエラーが発生しました:", error);
+      return c.json({ error: "データの取得中にエラーが発生しました", details: error as string }, 500);
+    }
+  },
+);
