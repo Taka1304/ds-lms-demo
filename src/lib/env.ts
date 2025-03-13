7d7e7c2aa020c023092fa189ae8e3b4e9a66837b
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2025-03-13 12:37:33 +0900

    ➕ add: jest

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    NODE_ENV: z.enum(["development", "production"]),
+    NODE_ENV: z.enum(["development", "production", "test"]),
     DATABASE_URL: z.string().url(),
     NEXTAUTH_URL: z.string().url(),
     NEXTAUTH_SECRET: z.string(),
