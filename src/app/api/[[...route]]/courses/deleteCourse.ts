53313891462b4bfb6944ab732f5cc78f4a16f28a
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2025-03-22 12:13:56 +0900

    update : コースがない時に404を返却. deleteメソッド成功時に204を返却

--- src/app/api/[[...route]]/courses/deleteCourse.ts
+++ src/app/api/[[...route]]/courses/deleteCourse.ts
@@ -4,6 +4,7 @@ import { createFactory } from "hono/factory";
 import type { Session } from "next-auth";
 import { z } from "zod";
 import { withAdmin } from "~/middleware/auth";
+import { recoverFromNotFound } from "~/utils";
 
 type Variables = {
   session: Session;
@@ -22,15 +23,21 @@ export const deleteCourse = factory.createHandlers(
   async (c) => {
     const { course_id } = c.req.valid("param");
     try {
-      await prisma.course.delete({
-        where: {
-          id: course_id,
-        },
-      });
-      const message = `コースID:${course_id}のコースが削除されました`;
-      return c.json({ course_id, message }, 200);
+      const course = await recoverFromNotFound(
+        prisma.course.delete({
+          where: {
+            id: course_id,
+          },
+        }),
+      );
+
+      if (!course) {
+        return c.notFound();
+      }
+
+      return c.body(null, 204);
     } catch (error) {
-      console.error("コースの削除中にエラーが発生しました:", error);
+      console.error(error);
       return c.json({ error: "コースの削除中にエラーが発生しました", details: error as string }, 500);
     }
   },
