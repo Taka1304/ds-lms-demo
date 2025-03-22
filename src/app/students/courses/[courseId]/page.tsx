6f33d82e672dac02b15db30bc251d919ffb8cada
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2025-03-22 18:14:15 +0900

    add : コースの問題一覧ページの作成

--- src/app/students/courses/[courseId]/page.tsx
+++ src/app/students/courses/[courseId]/page.tsx
@@ -1,96 +1,119 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { DataTable } from "@/components/ui/data-table";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Progress } from "@/components/ui/progress";
 import { Separator } from "@/components/ui/separator";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import type { ColumnDef } from "@tanstack/react-table";
+import Image from "next/image";
 import Link from "next/link";
-import { useMemo, useState } from "react";
 import { use } from "react";
 
-// TODO: dummy
-const allProblems = [
-  { id: "1", title: "Two Sum", difficultyLevel: 1, submitCount: 100, acceptCount: 80 },
-  { id: "2", title: "Reverse Integer", difficultyLevel: 2, submitCount: 80, acceptCount: 60 },
-  { id: "3", title: "Median of Two Sorted Arrays", difficultyLevel: 3, submitCount: 50, acceptCount: 20 },
-];
+type Problem = {
+  id: string;
+  title: string;
+  learner: number; // 学習者数
+  accuracy: number; // 正答率
+  average: number; // 平均点
+  problemId: string; // 問題のID(cuid)
+  completed: boolean; // 問題を解いたかどうか
+};
 
-const difficulties = ["全て", "簡単", "普通", "難しい"];
+function getProblems(): Problem[] {
+  /*TODO:
+    - この関数の引数にcourseIdを実装
+    - サーバーサイドからコースの問題データをフェッチしallProblemsに格納
+   */
+
+  // ダミーデータ
+  const allProblems: Problem[] = [];
+  for (let i = 0; i < 10; i++) {
+    allProblems.push({
+      id: i.toString(),
+      title: `${i}`,
+      learner: 1000,
+      accuracy: Math.min(90, 100), // 最大値 100
+      average: Math.min(80, 100), // 最大値 100
+      problemId: "uuid",
+      completed: i % 2 === 0,
+    });
+  }
+  return allProblems;
+}
+
+function getProgressValues(problems: Problem[]): {
+  progress: number;
+  completedProblems: number;
+  totalProblems: number;
+} {
+  const totalProblems = problems.length;
+  const completedProblems = problems.filter((problem) => problem.completed).length;
+  const progress = (completedProblems / totalProblems) * 100;
+  return { progress, completedProblems, totalProblems };
+}
 
 export default function ProblemList({ params }: { params: Promise<{ courseId: string }> }) {
-  const [filterDifficulty, setFilterDifficulty] = useState(difficulties[0]);
-  const filteredProblems =
-    filterDifficulty === difficulties[0]
-      ? allProblems
-      : allProblems.filter((p) => p.difficultyLevel === difficulties.indexOf(filterDifficulty));
-  const resolvedParams = use(params);
+  const { courseId } = use(params);
 
-  const columns: ColumnDef<(typeof allProblems)[0]>[] = useMemo(
-    () => [
-      {
-        accessorKey: "title",
-        header: "タイトル",
-      },
-      {
-        accessorKey: "difficultyLevel",
-        header: "難易度",
-        cell: ({ row }) => {
-          const problem = row.original;
-          return difficulties[problem.difficultyLevel];
-        },
-      },
-      {
-        accessorKey: "acceptance",
-        header: "正答率",
-        cell: ({ row }) => {
-          const problem = row.original;
-          return `${((problem.acceptCount / problem.submitCount) * 100).toFixed(2)}%`;
-        },
-      },
-      {
-        id: "actions",
-        cell: ({ row }) => {
-          const problem = row.original;
-          return (
-            <Link href={`/students/courses/${resolvedParams.courseId}/${problem.id}`}>
-              <Button size="sm" className="w-full">
-                挑戦する
-              </Button>
-            </Link>
-          );
-        },
-      },
-    ],
-    [resolvedParams.courseId],
-  );
+  const allProblems = getProblems();
+  const { progress, completedProblems, totalProblems } = getProgressValues(allProblems);
 
   return (
     <>
-      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+      <header className="flex h-16 items-center gap-2 ease-linear bg-[#5198de] bg-opacity-40">
         <div className="flex items-center gap-2 px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
-          <h2 className="text-md">演習問題</h2>
+          {/* TODO:日本語化 */}
+          <h2 className="text-3xl font-bold">DATA ANALISYS</h2>
         </div>
       </header>
-      <div className="space-y-6 container mx-auto px-4 py-8 min-h-[calc(100vh-60px)]">
-        <div className="flex justify-between items-center">
-          <Select onValueChange={setFilterDifficulty} defaultValue={filterDifficulty}>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue />
-            </SelectTrigger>
-            <SelectContent>
-              {difficulties.map((d) => (
-                <SelectItem key={d} value={d}>
-                  {d}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
+
+      <div className="space-y-6 container mx-auto px-4 py-8 min-h-[100vh-60px]">
+        <h2 className="text-sm font-bold text-black">問題集の進捗状況</h2>
+        <div className="flex flex-row items-center space-x-4">
+          <p className="text-center min-w-36 text-xl font-bold">
+            {completedProblems} / {totalProblems}問
+          </p>
+          <Image src={"/students/courses/AskingAQuestion2.png"} alt="AskingAQuestion2" width={96} height={102} />
+          <Progress value={progress} />
+          <Image src={"/students/courses/BeingVip.png"} alt="BeingVip" width={105} height={102} />
         </div>
-        <DataTable columns={columns} data={filteredProblems} />
-        {/* TODO: Pagination */}
+        {allProblems.map((problem) => (
+          <Card key={problem.id} className="flex flex-row space-around space-x-5">
+            <Image
+              className="p-6 min-w-min"
+              src={"/students/courses/AskingAQuestion.png"}
+              width={91}
+              height={101}
+              alt="sample"
+            />
+            <CardHeader className="w-full justify-around">
+              <CardTitle>問題: {problem.title}</CardTitle>
+              <CardDescription className="flex flex-row items-around space-x-10">
+                <span>受験者数: {problem.learner}人</span>
+                <span>正解率: {problem.accuracy}%</span>
+                <span>平均スコア: {problem.average} 点</span>
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="p-6 w-1/3 flex flex-col justify-center">
+              {/* TODO: ヒントボタンの挙動実装 */}
+              <Button className="bg-white border border-[#FF0000] rounded-none text-[#FF0000] w-full mb-3 text-xs min-h-5 max-h-7 hover:bg-gray-100 hover:font-bold">
+                <Image src={"/students/courses/Danger.png"} width={16} height={17} alt="DangerIcon" />
+                <span>ヒント</span>
+              </Button>
+              <div className="relative">
+                <Button asChild size="sm" className="w-full rounded-sm h-14 font-bold text-lg bg-[#5BBBE1] drop-shadow">
+                  <Link href={`/students/courses/${courseId}/${problem.problemId}`}>挑戦する</Link>
+                </Button>
+                {problem.completed ? (
+                  <div className="absolute inset-0 flex items-center justify-center opacity-90 hover:bg-green-200">
+                    <Image src={"/students/courses/checkIcon.png"} alt="checkIcon" width={36} height={36} />
+                  </div>
+                ) : null}
+              </div>
+            </CardContent>
+          </Card>
+        ))}
       </div>
     </>
   );
