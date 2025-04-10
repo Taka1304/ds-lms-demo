9c517a8ad03ca9f16b25d5b453fbabab8df571be
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2025-04-10 19:13:22 +0900

    :lipstick: ボタンとサイドバーに反映してみる

--- src/components/layout/sidebar/index.tsx
+++ src/components/layout/sidebar/index.tsx
@@ -17,7 +17,7 @@ import { NavUser } from "./user";
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
-    <Sidebar collapsible="icon" {...props}>
+    <Sidebar collapsible="icon" className="bg-primary/20 dark:bg-primary/40" {...props}>
       <SidebarHeader>
         <Link href="/students/home" className={"p-1"}>
           <Image src="/logo.png" alt="Logo" width={24} height={24} />
