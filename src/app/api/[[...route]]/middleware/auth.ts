58f8924b6fe1326f38f496963236c3d17a6a34df
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2025-04-14 14:27:33 +0900

    Chore🧹: ミドルウェアにコメントを追記

--- src/app/api/[[...route]]/middleware/auth.ts
+++ src/app/api/[[...route]]/middleware/auth.ts
@@ -5,6 +5,15 @@ import { getServerSession } from "next-auth";
 
 const factory = createFactory();
 
+/**
+ * @function getSession
+ * @description
+ * セッション情報を取得し、コンテキストに設定するミドルウェア。
+ * 次のミドルウェアを呼び出す前に、セッション情報を取得してコンテキストに保存します。
+ * 
+ * @param c - Honoのコンテキストオブジェクト
+ * @param next - 次のミドルウェアを呼び出すための関数
+ */
 export const getSession = factory.createMiddleware(async (c, next) => {
   const session = await getServerSession(authOptions);
 
@@ -12,6 +21,16 @@ export const getSession = factory.createMiddleware(async (c, next) => {
   await next();
 });
 
+/**
+ * @function withSession
+ * @description
+ * セッションが存在することを確認するミドルウェア。
+ * セッションが存在しない場合は、401 Unauthorizedエラーを返します。
+ * セッションが存在する場合は、コンテキストにセッション情報を設定し、次のミドルウェアを呼び出します。
+ * 
+ * @param c - Honoのコンテキストオブジェクト
+ * @param next - 次のミドルウェアを呼び出すための関数
+ */
 export const withSession = factory.createMiddleware(async (c, next) => {
   const session = await getServerSession(authOptions);
 
@@ -22,6 +41,17 @@ export const withSession = factory.createMiddleware(async (c, next) => {
   await next();
 });
 
+ /**
+ * @function withAdmin
+ * @description
+ * 管理者権限を確認するミドルウェア。
+ * セッションが存在しない場合は401 Unauthorizedエラーを返し、
+ * セッションが存在してもユーザーの役割が管理者でない場合は403 Forbiddenエラーを返します。
+ * 管理者権限が確認できた場合は、コンテキストにセッション情報を設定し、次のミドルウェアを呼び出します。
+ * 
+ * @param c - Honoのコンテキストオブジェクト
+ * @param next - 次のミドルウェアを呼び出すための関数
+ */
 export const withAdmin = factory.createMiddleware(async (c, next) => {
   const session = await getServerSession(authOptions);
   
