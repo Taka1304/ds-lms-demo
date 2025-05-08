ec0b6fff897bc109ac63c623c20206c012211590
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2025-05-08 16:14:21 +0900

    Fix: CoursesCarouselCardのCardコンポーネントのクラス名を修正

--- src/features/courses/components/CoursesCarouselCard.tsx
+++ src/features/courses/components/CoursesCarouselCard.tsx
@@ -14,7 +14,7 @@ export default function CoursesCarouselCard({
   const progressValue = (achievementLevel / maxAchievementLevel) * 100;
 
   return (
-    <Card className="w-full max-w-[255px] shadow-md rounded-rounded-t-2xl mb-11">
+    <Card className="w-full max-w-[255px] shadow-md rounded-t-2xl mb-11">
       <div className="relative w-full h-[184px]">
         <Image src={"/courseLogo.webp"} alt={title} fill className="object-cover rounded-t-2xl" />
       </div>
