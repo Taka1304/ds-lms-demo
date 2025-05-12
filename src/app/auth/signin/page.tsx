6ee8d5f3df88fda2c52160ca05e37e62125b0daf
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2025-05-12 16:30:10 +0900

    Fix🐛: 利用規約とプライバシーポリシーへのリンクを更新

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -54,8 +54,8 @@ export default async function SignInPage({
             {/* <SigninButton provider="github">Sign in with GitHub</SigninButton> */}
           </div>
           <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary">
-            By clicking continue, you agree to our <Link href="/terms-of-service">Terms of Service</Link> and{" "}
-            <Link href="/privacy-policy">Privacy Policy</Link>.
+            By clicking continue, you agree to our <Link href="/terms">Terms of Service</Link> and{" "}
+            <Link href="/privacy">Privacy Policy</Link>.
           </div>
         </div>
       </div>
