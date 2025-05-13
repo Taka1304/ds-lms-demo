0708e1f0f4646c39429c7ad74c7e1a33a078f703
yamaff <fuga.19671028@icloud.com>
2025-05-13 15:09:18 +0900

    feat:プロフィール設定機能追加

--- src/app/api/[[...route]]/users/index.ts
+++ src/app/api/[[...route]]/users/index.ts
@@ -2,6 +2,4 @@ import { Hono } from "hono";
 import { getUser } from "~/users/getUser";
 import { updateUser } from "~/users/updateUser";
 
-export const users = new Hono()
-  .get("/:user_id", ...getUser)
-  .patch("/:user_id", ...updateUser);
+export const users = new Hono().get("/:user_id", ...getUser).patch("/:user_id", ...updateUser);
