c004998c3da0cb1664bcdb3e9cd412d35324e6b0
Taka1304 <c1101036@planet.kanazawa-it.ac.jp>
2025-05-31 21:09:48 +0900

    Fix🐛: ユーザーの進捗を正しく取得し、表示を更新

--- src/features/dashboard/container/progress-overview.tsx
+++ src/features/dashboard/container/progress-overview.tsx
@@ -1,15 +1,22 @@
 "use server";
 
 import { ProgressOverview } from "@/features/dashboard/progress-overview";
+import { authOptions } from "@/lib/auth";
 import { client } from "@/lib/hono";
 import { AlertCircle } from "lucide-react";
+import { getServerSession } from "next-auth";
 import { cookies } from "next/headers";
 
 const ProgressOverviewContainer = async () => {
   const cookieStore = await cookies();
   const sessionToken = cookieStore.get("next-auth.session-token")?.value || "";
+  const session = await getServerSession(authOptions);
   const res = await client.api.courses.$get(
-    {},
+    {
+      query: {
+        user_id: session?.user.id || undefined,
+      },
+    },
     {
       headers: {
         Cookie: `next-auth.session-token=${sessionToken}`,
@@ -19,15 +26,19 @@ const ProgressOverviewContainer = async () => {
 
   if (!res.ok) {
     console.error("Failed to fetch course progress res:", res.statusText);
+    if (res.status === 401) {
+      console.error(`Unauthorized: ${sessionToken}`);
+    }
     return (
-      <div>
+      <div className="flex items-center justify-center">
         <AlertCircle />
         エラーが発生しました
       </div>
     );
   }
   const data = await res.json();
-  const courseProgress = data.filter((course) => course.UserProgress?.length);
+  const courseProgress = data.filter((course) => course.UserProgress.progress);
+
   if (courseProgress.length === 0) {
     return <div className="text-center text-muted-foreground">現在、進捗状況はありません。</div>;
   }
