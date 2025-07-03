3cea8d462b15991e8ee44818c0de72c36bdac42a
yamaff <fuga.19671028@icloud.com>
2025-07-03 14:25:57 +0900

    fix: レビュー修正

--- src/components/ui/markdown.tsx
+++ src/components/ui/markdown.tsx
@@ -6,10 +6,8 @@ import { cn } from "@/lib/utils";
 
 export function MarkdownViewer({ content, className }: { content: string; className?: string }) {
   return (
-    <div className={cn(className)}>
-      <Markdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]} className={cn(className)}>
-        {content}
-      </Markdown>
-    </div>
+    <Markdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]} className={cn(className)}>
+      {content}
+    </Markdown>
   );
 }
